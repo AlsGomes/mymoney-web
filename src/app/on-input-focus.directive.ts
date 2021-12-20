778187a84f999148b8d995239812803807673f4c
AlsGomes <als_08.net@hotmail.com>
2021-12-20 20:43:04 -0300

    Changed directive implementation

--- src/app/on-input-focus.directive.ts
+++ src/app/on-input-focus.directive.ts
@@ -1,20 +1,17 @@
-import { Directive, ElementRef, HostListener, Renderer2 } from '@angular/core';
+import { Directive, ElementRef, HostBinding, HostListener, Renderer2 } from '@angular/core';
 
 @Directive({
   selector: '[appOnInputFocus]'
 })
 export class OnInputFocusDirective {
 
-  constructor(
-    private elementRef: ElementRef,
-    private renderer: Renderer2
-  ) { }
+  @HostBinding('style.backgroundColor') backgroundColor: string = 'transparent';
 
   @HostListener('focus') onFocus() {
-    this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', '#e6f3f8');
+    this.backgroundColor = '#e6f3f8';
   }
 
   @HostListener('blur') onBlur() {
-    this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'transparent');
+    this.backgroundColor = 'transparent';
   }
 }
