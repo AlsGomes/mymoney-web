bb27270b26981dc3bc938f316ba96b81892afd70
AlsGomes <als_08.net@hotmail.com>
2021-12-20 15:52:00 -0300

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { TabViewModule } from 'primeng/tabview';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent    
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    TabViewModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
