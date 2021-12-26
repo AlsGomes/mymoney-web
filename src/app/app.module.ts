679724b561217b70e37a3c192a1aaf1aa8c0d92c
AlsGomes <als_08.net@hotmail.com>
2021-12-26 14:59:02 -0300

    Organized modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,7 @@
-import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { TranslateLoader, TranslateModule, TranslateService } from '@ngx-translate/core';
-import { TranslateHttpLoader } from '@ngx-translate/http-loader';
-import { ConfirmationService, MessageService } from 'primeng/api';
-import { ConfirmDialogModule } from 'primeng/confirmdialog';
-import { ToastModule } from 'primeng/toast';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
-import { PersonModule } from './person/person.module';
-import { PersonService } from './person/person.service';
-import { RegistryModule } from './registry/registry.module';
-import { RegistryService } from './registry/registry.service';
-
-export function HttpLoaderFactory(http: HttpClient): TranslateHttpLoader {
-  return new TranslateHttpLoader(http);
-}
 
 @NgModule({
   declarations: [
@@ -23,31 +9,9 @@ export function HttpLoaderFactory(http: HttpClient): TranslateHttpLoader {
   ],
   imports: [
     BrowserModule,
-
     CoreModule,
-    RegistryModule,
-    PersonModule,
-
-    ToastModule,
-    ConfirmDialogModule,
-
-    HttpClientModule,
-
-    TranslateModule.forRoot({
-      loader: {
-        provide: TranslateLoader,
-        useFactory: HttpLoaderFactory,
-        deps: [HttpClient]
-      }
-    }),
   ],
-  providers: [
-    RegistryService,
-    PersonService,
-    MessageService,
-    ConfirmationService,
-    TranslateService,
-  ],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,23 +1,66 @@
 import { CommonModule, DatePipe, registerLocaleData } from '@angular/common';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import localePt from '@angular/common/locales/pt';
 import { LOCALE_ID, NgModule } from '@angular/core';
+import { TranslateLoader, TranslateModule, TranslateService } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { ConfirmationService, MessageService } from 'primeng/api';
+import { ConfirmDialogModule } from 'primeng/confirmdialog';
+import { ToastModule } from 'primeng/toast';
+import { PersonModule } from '../person/person.module';
+import { PersonService } from '../person/person.service';
+import { RegistryModule } from '../registry/registry.module';
+import { RegistryService } from '../registry/registry.service';
 import { NavbarComponent } from './navbar/navbar.component';
 
 registerLocaleData(localePt, 'pt-BR');
+
+export function HttpLoaderFactory(http: HttpClient): TranslateHttpLoader {
+  return new TranslateHttpLoader(http);
+}
 
 @NgModule({
   declarations: [
     NavbarComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+
+    HttpClientModule,
+
+    RegistryModule,
+    PersonModule,
+
+    ToastModule,
+    ConfirmDialogModule,
+
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient]
+      }
+    }),
   ],
   exports: [
     NavbarComponent,
+    
+    ToastModule,
+    ConfirmDialogModule,
+    
+    RegistryModule,
+    PersonModule,
   ],
   providers: [
     DatePipe,
-    { provide: LOCALE_ID, useValue: 'pt-BR' }
+    { provide: LOCALE_ID, useValue: 'pt-BR' },
+
+    RegistryService,
+    PersonService,
+
+    MessageService,
+    ConfirmationService,
+    TranslateService,
   ]
 })
 export class CoreModule { }
