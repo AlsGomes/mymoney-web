7f523b5dd817a43142424e11a4a94648d3f59ba3
AlsGomes <als_08.net@hotmail.com>
2021-12-26 17:51:43 -0300

    Fetching persons in Registry Editing

--- src/app/person/person.service.ts
+++ src/app/person/person.service.ts
@@ -7,6 +7,11 @@ export interface PersonFilter {
   page: number;
 }
 
+export interface PersonSummary {
+  code: string;
+  name: string;
+}
+
 const baseURL = "http://localhost:8080/persons";
 const authorizationHeader = new HttpHeaders().append('Authorization', "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2NDA1NzIwOTAsInVzZXJfbmFtZSI6ImFkbWluQGFsZ2Ftb25leS5jb20iLCJhdXRob3JpdGllcyI6WyJST0xFX0NBREFTVFJBUl9DQVRFR09SSUEiLCJST0xFX1BFU1FVSVNBUl9QRVNTT0EiLCJST0xFX1JFTU9WRVJfUEVTU09BIiwiUk9MRV9DQURBU1RSQVJfTEFOQ0FNRU5UTyIsIlJPTEVfUEVTUVVJU0FSX0xBTkNBTUVOVE8iLCJST0xFX1JFTU9WRVJfTEFOQ0FNRU5UTyIsIlJPTEVfQ0FEQVNUUkFSX1BFU1NPQSIsIlJPTEVfUEVTUVVJU0FSX0NBVEVHT1JJQSJdLCJqdGkiOiJLM2tQUFd1eEhtTEpUQ3RxZGdnaTJpWFZRWVUiLCJjbGllbnRfaWQiOiJhbmd1bGFyIiwic2NvcGUiOlsicmVhZCIsIndyaXRlIl19.95mSDTAnFqa2OVCyJ5ICOkyLzaiYxNipIJRtRu4sCg8");
 
@@ -17,8 +22,9 @@ export class PersonService {
 
   constructor(private http: HttpClient) { }
 
-  async fetchAll(): Promise<any> {
-    return await this.http.get(baseURL, { headers: authorizationHeader }).toPromise()
+  async fetchAll(): Promise<PersonSummary[]> {
+    const res = await this.http.get<PersonSummary[]>(baseURL, { headers: authorizationHeader }).toPromise()
+    return res ?? []
   }
 
   async fetchByName(filter: PersonFilter): Promise<any> {
--- src/app/registry/registry-editing/registry-editing.component.ts
+++ src/app/registry/registry-editing/registry-editing.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Category, CategoryService } from 'src/app/category/category.service';
 import { ErrorHandlerService } from 'src/app/core/error-handler.service';
+import { PersonService, PersonSummary } from 'src/app/person/person.service';
 
 @Component({
   selector: 'app-registry-editing',
@@ -10,21 +11,33 @@ import { ErrorHandlerService } from 'src/app/core/error-handler.service';
 export class RegistryEditingComponent implements OnInit {
 
   categories: any[] = [];
+  persons: any[] = [];
   types = [{ label: 'RECEITA', value: 'INCOME' }, { label: 'DESPESA', value: 'EXPENSE' }];
-  persons = [{ label: 'Cristina', value: 'Cristina' }, { label: 'Alisson', value: 'Alisson' }];
 
   constructor(
-    private service: CategoryService,
+    private categoryService: CategoryService,
+    private personService: PersonService,
     private errorHandler: ErrorHandlerService) { }
 
   ngOnInit(): void {
     this.fetchCategories()
+    this.fetchPersons()
   }
 
   async fetchCategories() {
     try {
-      const res = await this.service.fetchAll();
+      const res = await this.categoryService.fetchAll();
       this.categories = res.map((cat: Category) => ({ label: cat.name, value: cat.code }))
+    } catch (err) {
+      this.errorHandler.handle(err)
+      console.log(err)
+    }
+  }
+
+  async fetchPersons() {
+    try {
+      const res = await this.personService.fetchAll();
+      this.persons = res.map((p: PersonSummary) => ({ label: p.name, value: p.code }))
     } catch (err) {
       this.errorHandler.handle(err)
       console.log(err)
