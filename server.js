0d11be34ceca887beaaa1bb30f1c279c65d287f2
AlsGomes <als_08.net@hotmail.com>
2021-12-30 18:50:14 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const express = require('express')
 const app = express()
 
-console.log(__dirname + '\\dist\\mymoney-web')
-app.use(express.static(__dirname + '\\dist\\mymoney-web'))
+console.log(__dirname + '/dist/mymoney-web')
+app.use(express.static(__dirname + '/dist/mymoney-web'))
 
 app.get('/*', function (req, res) {
-    res.sendFile(__dirname + '\\dist\\mymoney-web\\index.html')
+    res.sendFile(__dirname + '/dist/mymoney-web/index.html')
 })
 
 app.listen(process.env.PORT || 4200)
