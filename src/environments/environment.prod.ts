274c0675822849b137ad3bfeaafeaca26bf88961
AlsGomes <als_08.net@hotmail.com>
2021-12-30 17:55:35 -0300

    Ready to deploy in Heroku

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,8 @@
 export const environment = {
   production: true,
-  tokenAllowedDomains: [ /mymoney-api.herokuapp.com/ ],
   tokenDisallowedRoutes: [/\/oauth\/token/],
+  tokenAllowedDomains: [ /api.mymoney.net.br/ ],
   apiUrl: 'https://api.mymoney.net.br',
+  // apiUrl: 'http://localhost:8080',
+  // tokenAllowedDomains: [ /localhost:8080/ ],
 };
